8d27deb65083d9be928ce26e269f3617c80c7f10
FBronner <frederic.bronner@videotron.ca>
2020-08-31 15:40:46 -0400

    Fix a small issues with the tags cleanup not being done correctly

--- ASJTagsView/ASJTag.m
+++ ASJTagsView/ASJTag.m
@@ -39,77 +39,78 @@
 
 - (void)awakeFromNib
 {
-  self.crossImage = self.defaultCrossImage;
+    [super awakeFromNib];
+    self.crossImage = self.defaultCrossImage;
 }
 
 - (UIImage *)defaultCrossImage
 {
-  NSString *resourcesBundlePath = [self.tagsBundle pathForResource:@"Resources" ofType:@"bundle"];
-  NSBundle *resourcesBundle = [NSBundle bundleWithPath:resourcesBundlePath];
-  return [UIImage imageNamed:@"cross" inBundle:resourcesBundle compatibleWithTraitCollection:nil];
+    NSString *resourcesBundlePath = [self.tagsBundle pathForResource:@"Resources" ofType:@"bundle"];
+    NSBundle *resourcesBundle = [NSBundle bundleWithPath:resourcesBundlePath];
+    return [UIImage imageNamed:@"cross" inBundle:resourcesBundle compatibleWithTraitCollection:nil];
 }
 
 - (NSBundle *)tagsBundle
 {
-  return [NSBundle bundleForClass:[self class]];
+    return [NSBundle bundleForClass:[self class]];
 }
 
 #pragma mark - IBActions
 
 - (IBAction)tagTapped:(UIButton *)sender
 {
-  if (_tapBlock) {
-    _tapBlock(sender.titleLabel.text, self.tag);
-  }
+    if (_tapBlock) {
+        _tapBlock(sender.titleLabel.text, self.tag);
+    }
 }
 
 - (IBAction)deleteTapped:(UIButton *)sender
 {
-  if (_deleteBlock) {
-    _deleteBlock(_tagButton.titleLabel.text, self.tag);
-  }
+    if (_deleteBlock) {
+        _deleteBlock(_tagButton.titleLabel.text, self.tag);
+    }
 }
 
 #pragma mark - Property setters
 
 - (void)setTagText:(NSString *)tagText
 {
-  if (!tagText) {
-    return;
-  }
-  _tagText = tagText;
-  
-  [UIView performWithoutAnimation:^{
-    [_tagButton setTitle:tagText forState:UIControlStateNormal];
-    [_tagButton layoutIfNeeded];
-  }];
+    if (!tagText) {
+        return;
+    }
+    _tagText = tagText;
+    
+    [UIView performWithoutAnimation:^{
+        [self.tagButton setTitle:tagText forState:UIControlStateNormal];
+        [self.tagButton layoutIfNeeded];
+    }];
 }
 
 - (void)setTagTextColor:(UIColor *)tagTextColor
 {
-  if (!tagTextColor) {
-    return;
-  }
-  _tagTextColor = tagTextColor;
-  [_tagButton setTitleColor:tagTextColor forState:UIControlStateNormal];
+    if (!tagTextColor) {
+        return;
+    }
+    _tagTextColor = tagTextColor;
+    [_tagButton setTitleColor:tagTextColor forState:UIControlStateNormal];
 }
 
 - (void)setCrossImage:(UIImage *)crossImage
 {
-  if (!crossImage) {
-    return;
-  }
-  _crossImage = crossImage;
-  [_deleteButton setImage:crossImage forState:UIControlStateNormal];
+    if (!crossImage) {
+        return;
+    }
+    _crossImage = crossImage;
+    [_deleteButton setImage:crossImage forState:UIControlStateNormal];
 }
 
 - (void)setTagFont:(UIFont *)tagFont
 {
-  if (!tagFont) {
-    return;
-  }
-  _tagFont = tagFont;
-  _tagButton.titleLabel.font = tagFont;
+    if (!tagFont) {
+        return;
+    }
+    _tagFont = tagFont;
+    _tagButton.titleLabel.font = tagFont;
 }
 
 @end
